c75ce52cadde5b327eddd788e45ab44f633870e0
rafaelvm109 <75456178+rafaelvm109@users.noreply.github.com>
2022-08-24 13:57:48 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from urllib import response
 import nltk
 from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
 import numpy as np
 import tflearn
 import tensorflow as tf
@@ -8,13 +8,10 @@ import random
 import json
 import pickle
 
-stemmer = LancasterStemmer()
-
-with open('intents.json') as file:
+with open("intents.json") as file:
     data = json.load(file)
 
-# so you don't have to run all the training every time
-try: 
+try:
     with open("data.pickle", "rb") as f:
         words, labels, training, output = pickle.load(f)
 except:
@@ -63,12 +60,12 @@ except:
     training = np.array(training)
     output = np.array(output)
 
-    with open("data.pickle", "rb") as f:
+    with open("data.pickle", "wb") as f:
         pickle.dump((words, labels, training, output), f)
 
 # M O D E L
 # resets previous info
-tf.reset_default_graph()
+tf.compat.v1.reset_default_graph()
 # define input shape for the model
 net = tflearn.input_data(shape=[None, len(training[0])])
 # add fully connected layer with 8 neurons
