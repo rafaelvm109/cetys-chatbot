167ff68f34d9c7ccacafca868e97f9d711e771f0
Ernesto Sanchez <esanchez98@protonmail.com>
2022-11-06 11:46:11 -0800

    added read functionality

--- ChatbotServer/urls.py
+++ ChatbotServer/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('DBView/', include('DBView.urls')),
+    path('', include('DBView.urls')),
     path('ChatView/', include('ChatView.urls'))
 ]
--- DBView/urls.py
+++ DBView/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('category/<category_id>', views.category, name="category")
 ]
