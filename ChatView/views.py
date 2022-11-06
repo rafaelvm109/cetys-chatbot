9f1b5ab2a6c5e082e10dc3cfe9fd64b76a21a383
Ernesto Sanchez <esanchez98@protonmail.com>
2022-11-05 20:56:06 -0700

    added new version of DB View

--- ChatView/views.py
+++ ChatView/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "ChatIndex.html")
+
+
+# Get User Input
+def user_input(input):
+    text = input
